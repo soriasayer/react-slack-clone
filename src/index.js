0322e76feb9f472d055dd30b35189d06f9777a86
Soria <soriasayer@gmail.com>
2020-03-29 22:24:31 +0200

    Adding Spinner component and connecting data to the redux.

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ import {
 } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
+import { setUser } from "./actions/index";
+import Spinner from "./Spinner";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
@@ -23,14 +25,16 @@ class Root extends Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
   }
 
   render() {
-    return (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route path="/" exact component={App} />
         <Route path="/login" component={Login} />
@@ -40,7 +44,13 @@ class Root extends Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const mapStateToProps = ({ user }) => {
+  return {
+    isLoading: user.isLoading
+  };
+};
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
 
 ReactDOM.render(
   <Provider store={store}>
