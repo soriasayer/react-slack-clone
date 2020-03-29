397ab5b2d4f7007375d99a6c55ba00dd61e5de0b
Soria <soriasayer@gmail.com>
2020-03-29 16:01:49 +0200

    Seted up actions, actions types and reducers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import firebase from "./firebase";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+const store = createStore(rootReducer, composeWithDevTools());
 
-import 'semantic-ui-css/semantic.min.css'
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // console.log(user);
+        this.props.history.push("/");
+      }
+    });
+  }
 
-const Root = () => (
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Provider store={store}>
     <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
+      <RootWithAuth />
     </Router>
-)
+  </Provider>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
